8af17e304a50cde3fe4e3f8cb6ac2376ef4a7dff
Alex Lew <alew104@gmail.com>
2015-07-27 19:32:09 -0700

    read address from csv

--- geoLocate.py
+++ geoLocate.py
@@ -6,11 +6,17 @@ def openCSV(fileName):
             reader = csv.DictReader(infile)
             fnames = reader.fieldnames + ['latitude', 'longitude', 'county']
             print(fnames)
-            write = csv.DictWriter(outfile)
-            write.writerow(fnames)
+            write = csv.DictWriter(outfile, fnames = fnames)
+            write.writeheader(fnames)
             print('wrote header')
             for row in reader:
-                print(row)
+                address = row['address']
+                city = row['city']
+                state = row['state']
+                zipcode = row['zip']
+                fullAddress = address + ' ' + city + ' ' + state + ' ' + zipcode
+                print(fullAddress)
+                findLocate(fullAddress)
 
         #findLocate()
         #dwrite = csv.DictWriter(outfile)
@@ -18,9 +24,11 @@ def openCSV(fileName):
 
 
 
-def findLocate():
-    from geopy.geocoders import GoogleV3
-    geolocator = GoogleV3
+def findLocate(fullAddress):
+    from geopy.geocoders import Bing
+    geolocator = Bing(api_key = 'AiLKeLoEcoIYjSYqbfCmHPTzpMa2m_UWqGuNehGs-Kf6xRKo3Yl0t4HKm2CEAcrA')
+    loc = geocoder.geocode(fullAddress)
+    print(loc.raw)
 
 
 
