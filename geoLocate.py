559017008d17e71e8758ff86917f7168e7041d9d
Alex Lew <alew104@gmail.com>
2015-07-27 18:55:22 -0700

    init commit

---
+++ geoLocate.py
@@ -0,0 +1,21 @@
+import csv
+
+fileName = raw_input("Enter new filename")
+openCSV(fileName)
+
+def openCSV(fileName):
+    with open('addresses.csv') as infile
+        with open(fileName + '.csv') as outfile
+        dreader = csv.DictReader(infile)
+        for row in dreader:
+            print(row)
+
+        #findLocate()
+        #dwrite = csv.DictWriter(outfile)
+
+
+
+
+def findLocate():
+    from geopy.geocoders import GoogleV3
+    geolocator = GoogleV3
